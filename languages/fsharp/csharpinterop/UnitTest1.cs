e15dffdd356c863a2d77fec38f22cea63284d8c4
Aaron Westre <aaron@artificialnatures.com>
2019-07-06 19:47:21 -0500

    added fsharp

---
+++ languages/fsharp/csharpinterop/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace csharpinterop
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ languages/fsharp/reactive/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace reactive
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
