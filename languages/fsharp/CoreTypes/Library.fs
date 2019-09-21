290e272a745fdb0a4ed321e6f5047705cd8a0f4a
Aaron Westre <aaron@artificialnatures.com>
2019-09-21 17:45:29 -0500

    eh

---
+++ languages/fsharp/CoreTypes/Library.fs
@@ -0,0 +1,31 @@
+﻿namespace CoreTypes
+
+type Behavior = float -> float
+
+type BehaviorBuilder = interface end
+
+type WaveBehavior =
+    | Sine
+    | Triangle
+    | Sawtooth
+    interface BehaviorBuilder
+
+type BehaviorCollection = interface end
+
+type ScalarBehavior =
+    | Wave
+    | Envelope
+    | Scale
+    interface BehaviorCollection
+
+type Topic = interface end
+
+type CoreTopic =
+    | Scalar
+    | Vector
+    | Matrix
+    interface Topic
+    
+module topic =
+    let topicToString (topic : Topic) = topic.ToString()
+    
---
+++ languages/fsharp/ExtendedTypes/Library.fs
@@ -0,0 +1,25 @@
+﻿namespace ExtendedTypes
+
+open CoreTypes
+
+type CharacterBehavior =
+    | A
+    | B
+    | C
+    interface BehaviorBuilder
+
+type WordBehavior =
+    | Long
+    | Medium
+    | Short
+    interface BehaviorCollection
+
+type ExtendedTopic =
+    | Words
+    | Sentences
+    | Paragraphs
+    interface Topic
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
